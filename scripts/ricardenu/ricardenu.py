808ec5e9a2258889684c7213563c5669f6a2f233
enumivo <admin@enumivo.org>
2018-08-15 16:03:48 +0800

    rename eos

--- scripts/ricardenu/ricardenu.py
+++ scripts/ricardenu/ricardenu.py
@@ -133,13 +133,13 @@ def export_ricardian_from_abi(source_abi):
 
 def main():
     if len(sys.argv) == 1:
-        print('Please specify an operation of export or import: ./ricardeos.py <import|export>')
+        print('Please specify an operation of export or import: ./ricardenu.py <import|export>')
         sys.exit(1)
 
     if sys.argv[1] == 'import':
         if len(sys.argv) != 4:
             print('Please specify a source and destination abi:')
-            print('Usage: ./ricardeos.py import /eos/contracts/contract/mycontract.abi /eos/contracts/contract/withricardian-mycontract.abi')
+            print('Usage: ./ricardenu.py import /enumivo/contracts/contract/mycontract.abi /enumivo/contracts/contract/withricardian-mycontract.abi')
 
             sys.exit(0)
         else:
@@ -149,7 +149,7 @@ def main():
     elif sys.argv[1] == 'export':
         if len(sys.argv) != 3:
             print('Please specify a source abi:')
-            print('Usage: ./ricardeos.py export /eos/contracts/contract/mycontract.abi')
+            print('Usage: ./ricardenu.py export /enumivo/contracts/contract/mycontract.abi')
 
             sys.exit(0)
         else:
