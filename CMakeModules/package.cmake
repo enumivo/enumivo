d84c3c20b0e12954f794c277cfd1629a049c8b46
enumivo <admin@enumivo.org>
2018-10-17 12:19:14 +0800

    edit package

--- CMakeModules/package.cmake
+++ CMakeModules/package.cmake
@@ -1,8 +1,8 @@
-set(VENDOR "block.one")
-set(PROJECT_NAME "eosio")
-set(DESC "Software for the EOS.IO network")
-set(URL "https://github.com/eosio/eos")
-set(EMAIL "support@block.one")
+set(VENDOR "enumivo.org")
+set(PROJECT_NAME "enumivo")
+set(DESC "Software for the Enumivo network")
+set(URL "https://github.com/enumivo/enumivo")
+set(EMAIL "admin@enumivo.org")
 
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_package.sh.in ${CMAKE_BINARY_DIR}/packages/generate_package.sh @ONLY)
 configure_file(${CMAKE_SOURCE_DIR}/scripts/generate_bottle.sh ${CMAKE_BINARY_DIR}/packages/generate_bottle.sh COPYONLY)
