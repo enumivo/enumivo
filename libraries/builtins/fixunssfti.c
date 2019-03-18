20aa1b28818cd3802fe70ae2f625ce17c369dfde
quattrocento <46367535+quattrocento@users.noreply.github.com>
2019-03-18 22:06:10 +0800

    LICENSE

--- libraries/builtins/fixunssfti.c
+++ libraries/builtins/fixunssfti.c
@@ -3,7 +3,7 @@
  *                     The LLVM Compiler Infrastructure
  *
  * This file is dual licensed under the MIT and the University of Illinois Open
- * Source Licenses. See LICENSE.TXT for details.
+ * Source Licenses. See LICENSE for details.
  *
  * ===----------------------------------------------------------------------===
  *
